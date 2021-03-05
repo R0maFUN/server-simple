46c0de935393c365066a745cf930cc3934defde7
Konstantin Morozov <konstantin.morozov@openmedia.co>
2021-03-05 07:03:35 +0300

    add files

---
+++ main.py
@@ -0,0 +1,18 @@
+from network import run_connection
+import asyncio
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    run_accepts = asyncio.start_server(
+        client_connected_cb=run_connection, host="127.0.0.1", port=8018, loop=loop
+    )
+    try:
+        server = loop.run_until_complete(run_accepts)
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print("Stopped...")
+    finally:
+        server.close()
+        loop.run_until_complete(server.wait_closed())
+        loop.close()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="server",
+    version="0.0.1",
+    packages=["network"],
+    url="",
+    license="",
+    author="Konstantin Morozov",
+    author_email="morozov-kst@yandex.ru",
+    description="Server for GoodOK",
+)
