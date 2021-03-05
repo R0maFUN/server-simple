61cbc31f16fc04d2f3c9167c8b0878104a8d13d1
Konstantin Morozov <konstantin.morozov@openmedia.co>
2021-03-05 07:19:54 +0300

    test pre-commit fix fix

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from network import run_connection
 import asyncio
+from network import run_connection
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     run_accepts = asyncio.start_server(
-        client_connected_cb=run_connection, host="127.0.0.1", port=8018, loop=loop
+        client_connected_cb=run_connection,
+        host="127.0.0.1",
+        port=8018,
+        loop=loop,
     )
+
     try:
         server = loop.run_until_complete(run_accepts)
         loop.run_forever()
