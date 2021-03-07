d0654fa89f686cceb7f72f1988f7d1f3d88aab98
Konstantin Morozov <konstantin.morozov@openmedia.co>
2021-03-07 08:54:18 +0300

    some refactoring

--- server/app.py
+++ server/app.py
@@ -1,11 +1,8 @@
 """
     @TODO write docs
 """
-import asyncio
-from functools import partial
 import argparse
-from server.network.connections import handle_connection
-from server.network.sendbacker import SendBacker
+from server.network.HandlerConnects import HandlerConnects
 
 
 def parse_args() -> dict:
@@ -24,31 +21,9 @@ def parse_args() -> dict:
 
 
 def main():
-    """
-    @TODO write docs
-    """
     args = parse_args()
-
-    loop = asyncio.get_event_loop()
-    sender = SendBacker()
-    handler = partial(handle_connection, sender=sender)
-    run_accepts = asyncio.start_server(
-        client_connected_cb=handler,
-        host=args["address"],
-        port=args["port"],
-        loop=loop,
-    )
-    print("start server")
-    try:
-        server = loop.run_until_complete(run_accepts)
-        loop.run_forever()
-    except KeyboardInterrupt:
-        print("Stopped...")
-    finally:
-        server.close()
-        loop.run_until_complete(server.wait_closed())
-        loop.close()
-        print("Close server")
+    server = HandlerConnects(args)
+    server.run()
 
 
 if __name__ == "__main__":
